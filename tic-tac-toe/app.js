3e5cd4d52584dad7ba5eb849743c15fcf624f7dd
haliilozz <halil447oz@gmail.com>
2023-10-06 01:08:43 +0300

    modalopen

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -9,9 +9,22 @@ const selectionArticle = document.querySelector(".selection");
 //?secilen elemamların taşıyıcıları
 const yourChoiceDiv = document.getElementById("your-choice");
 const pcChoiceDiv = document.getElementById("pc-choice");
+//?message
+const messagePar = document.querySelector(".message");
+//?score
+const scoreCardSection = document.querySelector(".score-card");
+const pcScoreSpan = document.getElementById("pc-score");
+const yourScoreSpan = document.getElementById("your-score");
+//?modal
+const modalCardSection = document.querySelector(".modal-card");
+
 //*--------------variables
 let userSelectImg = document.createElement("img");
 let pcSelectImg = document.createElement("img");
+//?COLORS
+const YELLOW = "#ffc538";
+const RED = "#fb778b";
+const GREEN = "#5ab7ac";
 //*--------------event listeners
 selectionArticle.addEventListener("click", (e) => {
   if (e.target.id) {
@@ -21,24 +34,52 @@ selectionArticle.addEventListener("click", (e) => {
     createPcSelection();
   }
 });
+//*--------------functions
 const createPcSelection = () => {
   const pcArr = ["rock", "paper", "scissor"];
   const pcRandom = pcArr[Math.floor(Math.random() * 3)];
   pcSelectImg.src = `./assets/${pcRandom}.png`;
   pcSelectImg.alt = pcRandom;
   pcChoiceDiv.appendChild(pcSelectImg);
+  calculateResult();
 };
-// rockImg.addEventListener("click", () => {
-//   image.src = "./assets/rock.png";
-//   yourChoiceDiv.appendChild(image);
-// });
-// paperImg.addEventListener("click", () => {
-//   image.src = "./assets/paper.png";
-//   yourChoiceDiv.appendChild(image);
-// });
-// scissorImg.addEventListener("click", () => {
-//   image.src = "./assets/scissor.png";
-//   yourChoiceDiv.appendChild(image);
-// });
 
-//*--------------functions
+const calculateResult = () => {
+  //?eşitlik durumu
+  if (userSelectImg.alt === pcSelectImg.alt) {
+    draw();
+  } else {
+    if (userSelectImg.alt === "rock") {
+      pcSelectImg.alt === "paper" ? youLost() : youWin();
+    } else if (userSelectImg.alt === "scissor") {
+      pcSelectImg.alt === "rock" ? youLost() : youWin();
+    } else if (userSelectImg.alt === "paper") {
+      pcSelectImg.alt === "scissor" ? youLost() : youWin();
+    }
+  }
+  if (pcScoreSpan.textContent === "10" || yourScoreSpan.textContent === "10") {
+    openModal();
+  }
+};
+const draw = () => {
+  messagePar.textContent = "Its a draw";
+  scoreCardSection.style.color = YELLOW;
+  messagePar.style.backgroundColor = YELLOW;
+};
+
+const youLost = () => {
+  messagePar.textContent = "you lost";
+  scoreCardSection.style.color = RED;
+  messagePar.style.backgroundColor = RED;
+  pcScoreSpan.textContent++;
+};
+const youWin = () => {
+  messagePar.textContent = "you WİN";
+  scoreCardSection.style.color = GREEN;
+  messagePar.style.backgroundColor = GREEN;
+  yourScoreSpan.textContent++;
+};
+
+const openModal = () => {
+  modalCardSection.classList.add("show");
+};
