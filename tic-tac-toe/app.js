6e0ad3c3a5bb11fb81799898fbffb7ae48dab291
haliilozz <halil447oz@gmail.com>
2023-10-05 23:20:02 +0300

    pc choice

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -8,18 +8,26 @@ const scissorImg = document.getElementById("scissor");
 const selectionArticle = document.querySelector(".selection");
 //?secilen elemamların taşıyıcıları
 const yourChoiceDiv = document.getElementById("your-choice");
-
+const pcChoiceDiv = document.getElementById("pc-choice");
 //*--------------variables
 let userSelectImg = document.createElement("img");
-
+let pcSelectImg = document.createElement("img");
 //*--------------event listeners
 selectionArticle.addEventListener("click", (e) => {
-  console.log(e.target.id);
   if (e.target.id) {
     userSelectImg.src = `./assets/${e.target.id}.png`;
+    userSelectImg.alt = e.target.id;
     yourChoiceDiv.appendChild(userSelectImg);
+    createPcSelection();
   }
 });
+const createPcSelection = () => {
+  const pcArr = ["rock", "paper", "scissor"];
+  const pcRandom = pcArr[Math.floor(Math.random() * 3)];
+  pcSelectImg.src = `./assets/${pcRandom}.png`;
+  pcSelectImg.alt = pcRandom;
+  pcChoiceDiv.appendChild(pcSelectImg);
+};
 // rockImg.addEventListener("click", () => {
 //   image.src = "./assets/rock.png";
 //   yourChoiceDiv.appendChild(image);
